c75fa16f31fb05efb19089add9623cce71355167
Zcaic <462845148@qq.com>
2025-11-29 20:40:20 +0800

    test sympy complex

--- test/test01.py
+++ test/test01.py
@@ -68,7 +68,59 @@ def test3():
     print(func(anp.array([1, 3, 3, 3, 5])))
 
 
+def test4():
+    opti = cas.Opti()
+
+    # 声明变量K并设置为整数类型
+    K = opti.variable()
+    opti.set_domain(K,'integer')
+
+    # 定义数组并转换为MX类型
+    A = [3,2,1,2,3]
+    A = cas.MX(A)
+
+    # 构建目标函数：最小化A[K]
+    opti.minimize(A[K])
+
+    # 添加约束：确保K在数组有效范围内
+    opti.subject_to(0 <= (K <= 4))
+
+    # 设置求解器并求解
+    opti.solver("bonmin")
+    sol = opti.solve()
+
+    # 输出结果
+    print(sol.value(K))
+
+def test5():
+    import sympy as sp 
+
+    # 定义变量 p（复数）
+    p = sp.symbols('p', complex=True)
+
+    # 定义 2×2 矩阵（元素可以是 p 的函数）
+    # 示例矩阵：A = [[p, 1], [1, p]]
+    A = sp.Matrix([
+        [p, -1],
+        [1, p]
+    ])
+
+    # 计算行列式
+    det_A = A.det()
+
+    # 求解行列式 = 0 的方程
+    # solutions = sp.solve(det_A, p)
+    solutions = sp.nsolve(det_A, p,-1j)
+    print(solutions.evalf())
+
+    # 打印解
+    # print("行列式为 0 的解：")
+    # for sol in solutions:
+    #     print(f"p = {sol.evalf()}")  # 数值化输出
+
 if __name__ == "__main__":
     # test1()
-    test2()
+    # test2()
     # test3()
+    # test4()
+    test5()
